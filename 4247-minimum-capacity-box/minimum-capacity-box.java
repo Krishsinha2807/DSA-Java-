75f0496f1e4f5476c45f09b4b5f51de5b86b1496
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-04-29 14:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (72.99%) - LeetSync

--- 4247-minimum-capacity-box/minimum-capacity-box.java
+++ 4247-minimum-capacity-box/minimum-capacity-box.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minimumIndex(int[] capacity, int itemSize) {
-        int min=Integer.MAX_VALUE;
+       /* int min=Integer.MAX_VALUE;
         for(int i=0;i<capacity.length;i++){
             if(capacity[i]>=itemSize){
                min=Math.min(capacity[i],min);
@@ -14,6 +14,15 @@ class Solution {
             if(capacity[i]==min) {
                 idx=i;
                 break;
+            }
+        }
+        return idx;*/
+        int min = Integer.MAX_VALUE;
+        int idx = -1;
+        for (int i = 0; i < capacity.length; i++) {
+            if (capacity[i] >= itemSize && capacity[i] < min) {
+                min = capacity[i];
+                idx = i;
             }
         }
         return idx;
