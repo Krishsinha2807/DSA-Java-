8c2b1586701a1f82ef57009cef8343d51756b1d2
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-05-17 13:32:56 +0530

    Time: 3 ms (82.54%) | Memory: 43.3 MB (81.01%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,23 +1,22 @@
 class Solution {
     public int calPoints(String[] operations) {
-        Stack<Integer>stack =new Stack<>();
-        int sum=0;
+        Stack<Integer>stack=new Stack<>();
         for(int i=0;i<operations.length;i++){
-            String t=operations[i];
-            if(!(t.equals("C")|| t.equals("D")||t.equals("+"))){
-               stack.push(Integer.parseInt(t));
+            String temp=operations[i];
+            if(!(temp.equals("+") || temp.equals("D")||temp.equals("C"))){
+                stack.push(Integer.parseInt(temp));
             }
-            else if(t.equals("C")) stack.pop();
-            else if(t.equals("D")) stack.push(2 * stack.peek());
-            else if(t.equals("+")){
-                int top =stack.pop();
-                int nsum=top+stack.peek();
-                stack.push(top);
+            else if(temp.equals("+")){
+                int a=stack.pop();
+                int nsum=a+stack.peek();
+                stack.push(a);
                 stack.push(nsum);
             }
-        }
+            else if(temp.equals("D")) stack.push(2*stack.peek());
+            else if(temp.equals("C")) stack.pop();
+        } int sum=0;
         for(int i=0;i<stack.size();i++){
-            sum=sum+stack.get(i);
+             sum=sum+stack.get(i);
         }
         return sum;
         
