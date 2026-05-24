e46727731dd529c156acde19e0602f14b9866290
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-05-24 23:06:54 +0530

    Time: 15 ms (40.50%) | Memory: 47 MB (85.73%) - LeetSync

---
+++ 4313-password-strength/password-strength.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int passwordStrength(String password) {
+        int count=0;
+        HashSet<Character>set=new HashSet<>();
+        for(int i=0;i<password.length();i++){
+            char ch=password.charAt(i);
+             set.add(ch);
+        }
+        char arr[]=new char[set.size()];
+        int idx=0;
+        for(char ch:set){
+            arr[idx]=ch;
+            idx++;
+        }
+        for(int i=0;i<set.size();i++){
+            char ch=arr[i];
+            if(ch>='A' && ch<='Z') count=count+2;
+            else if(ch>='a' && ch<='z') count =count+1;
+            else if(Character.isDigit(ch)) count =count +3;
+            else count =count +5;
+
+        }
+        return count;
+       /* for(int i=0;i<password.length();i++){
+            char ch=password.charAt(i);
+            if(ch>='A' && ch<='Z') count=count+2;
+            else if(ch>='a' && ch<='z') count =count+1;
+            else if(Character.isDigit(ch)) count =count +3;
+            else count =count +5;
+
+        }
+        return count;*/
+        
+    }
+}
