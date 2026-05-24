274f998e68203f03dfbf782a76e56b8218252a36
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-05-24 23:55:38 +0530

    Time: 2 ms (4.82%) | Memory: 44.3 MB (11.34%) - LeetSync

--- 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.java
+++ 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.java
@@ -5,14 +5,14 @@ class Solution {
             char ch=s.charAt(i);
            list.add((ch-'0'));
         }
-        int arr[]=new int[list.size()];
+        int dp[]=new int[list.size()];
         int idx=0;
         for(int i=0;i<list.size();i++){
-            arr[idx]=list.get(i);
+            dp[idx]=list.get(i);
             idx++;
         }
-        for(int i=1;i<arr.length;i++){
-            if(!(Math.abs(arr[i]-arr[i-1])<=2))  return false;
+        for(int i=1;i<dp.length;i++){
+            if(!(Math.abs(dp[i]-dp[i-1])<=2))  return false;
         }
         return true;
         
