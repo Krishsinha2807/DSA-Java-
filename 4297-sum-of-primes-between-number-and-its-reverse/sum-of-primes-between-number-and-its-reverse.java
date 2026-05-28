74975da2cdab95436d841babde93fdfbb8480d93
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-05-28 16:33:42 +0530

    Time: 274 ms (5.00%) | Memory: 42.5 MB (78.49%) - LeetSync

--- 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.java
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.java
@@ -1,23 +1,25 @@
 class Solution {
     public int sumOfPrimesInRange(int n) {
-        int rev=0;
+        int r=0;
         int n1=n;
         while(n>0){
             int d=n%10;
-            rev=rev*10+d;
+            r=r*10+d;
             n=n/10;
-        } int count=0;
-        int sum=0;
-        int start =Math.min(n1,rev);
-        int end=Math.max(n1,rev);
-        for(int i=start;i<=end;i++){
-            int temp=i;
-            count =0;
-            for(int j=1;j<=i;j++){
-                if(i%j==0) count ++;
-            }
-            if(count ==2) sum=sum+i;
-        }
+        } int sum=0;
+        int max=Math.max(n1,r);
+        int min=Math.min(n1,r);
+        for(int i=min;i<=max;i++){
+            if(isPrime(i)) sum=sum+i;
+        }   
         return sum;
+    }
+    boolean isPrime(int x){
+        int count=0;
+        for(int j=1;j<=x;j++){
+            if(x%j==0) count ++;
+        }
+        if(count==2) return true;
+        return false;
     }
 }
