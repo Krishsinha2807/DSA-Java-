850551aaf9fcaeee7b704652315f76fb3c6d8434
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-05-29 10:19:06 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (27.04%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
@@ -14,8 +14,13 @@ class Solution {
             arr[idx]=sum;
             idx++;
         }
-        Arrays.sort(arr);
-        return arr[0];
+        /*Arrays.sort(arr);
+        return arr[0];*/
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min) min=arr[i];
+        }
+        return min;
         
     }
 }
