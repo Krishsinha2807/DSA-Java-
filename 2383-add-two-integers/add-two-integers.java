8f72a01d285992026d358df44b42462d88d68139
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-06-01 10:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (71.53%) - LeetSync

--- 2383-add-two-integers/add-two-integers.java
+++ 2383-add-two-integers/add-two-integers.java
@@ -1,9 +1,7 @@
 class Solution {
     public int sum(int num1, int num2) {
         if (num2 == 0) return num1;
-        if (num2 > 0) {
-            return sum(num1 + 1, num2 - 1);
-        }
+        if (num2 > 0) return sum(num1 + 1, num2 - 1);
         return sum(num1 - 1, num2 + 1);
     }
 }
