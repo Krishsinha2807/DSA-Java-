ace657a9148ec38ef615ec83ac2c4ff5f505b833
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-06-04 10:32:10 +0530

    Time: 22 ms (89.98%) | Memory: 77.4 MB (91.32%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,32 +1,15 @@
-/*class Solution {
+class Solution {
     public int longestConsecutive(int[] nums) {
+        if(nums.length == 0) return 0;
         Arrays.sort(nums);
         int count=1;
-        int max=0;
+        int max=1;
         if(nums.length == 0) return 0;
         for(int i=1;i<nums.length;i++){
             if(nums[i]==nums[i-1]) continue;
             if(nums[i]==nums[i-1]+1) count++;
             else count=1;
             max=Math.max(max,count);
-        }
-        return max;
-    }
-}*/
-class Solution {
-    public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) return 0;
-        Arrays.sort(nums);
-        int count = 1;
-        int max = 1;
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] == nums[i-1])
-                continue;
-            if(nums[i] == nums[i-1] + 1)
-                count++;
-            else
-                count = 1;
-            max = Math.max(max, count);
         }
         return max;
     }
