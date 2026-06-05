1921b91cdf22bad1635b865950003fb09e437584
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-06-05 10:24:10 +0530

    Time: 1 ms (80.25%) | Memory: 45.6 MB (57.01%) - LeetSync

--- 3246-check-if-bitwise-or-has-trailing-zeros/check-if-bitwise-or-has-trailing-zeros.java
+++ 3246-check-if-bitwise-or-has-trailing-zeros/check-if-bitwise-or-has-trailing-zeros.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean hasTrailingZeros(int[] nums) {
+        /*for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums.length; j++){
+                if(i!=j&&(nums[i]|nums[j])%2==0){
+                    return true;
+                }
+            }
+        }*/
         int c=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]%2==0) c++;
