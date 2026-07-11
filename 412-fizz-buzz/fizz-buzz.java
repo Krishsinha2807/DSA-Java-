13d23b3a20f6f0d5da0a0786bfb856159562c867
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-07-11 15:19:13 +0530

    Time: 2 ms (15.46%) | Memory: 47.1 MB (11.64%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -4,9 +4,7 @@ class Solution {
         for(int i=1;i<=n;i++)
         {
             if(i%3==0 && i%5==0) list.add("FizzBuzz");
-            
             else if(i%3==0) list.add("Fizz");
-            
             else if(i%5==0) list.add("Buzz");
             else{
                 String str=""+i;
