6257768bff8e93ca804c23ab7644e1be7b7f16d1
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-07-16 08:38:35 +0530

    Time: 2 ms (97.99%) | Memory: 43.6 MB (53.08%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -6,13 +6,11 @@ class Solution {
         for(int i = 0; i < arr.length; i++) {
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-
         HashSet<Integer> set = new HashSet<>();
         for(int freq : map.values()) {
             if(set.contains(freq)) return false;
             set.add(freq);
         }
-
         return true;
     }
 }
