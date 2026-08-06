d726be89f10989ad67060b5c60b51449ed01d5c9
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-08-06 08:54:44 +0530

    Time: 32 ms (81.25%) | Memory: 76.2 MB (7.46%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -7,6 +7,32 @@ class Solution {
             des[idx]=nums[i];
             idx++;
         } 
-        return des[k-1]; 
+        return des[k-1];
+    }}
+        /*int target = nums.length - k;
+        return quickSelect(nums,0,nums.length-1,target);
     }
-}
+    private int quickSelect(int[] nums,int low,int high,int target) {
+        int pivotIndex=partition(nums, low,high);
+        if (pivotIndex==target)return nums[pivotIndex];
+        if (pivotIndex>target)return quickSelect(nums,low,pivotIndex-1,target);
+        return quickSelect(nums, pivotIndex + 1, high, target);
+    }
+    private int partition(int[] nums,int low,int high) {
+        int pivot=nums[high];
+        int i=low;
+        for(int j=low;j<high;j++) {
+            if (nums[j]<=pivot) {
+                swap(nums,i,j);
+                i++;
+            }
+        }
+        swap(nums,i,high);
+        return i;
+    }
+    private void swap(int[] nums,int i,int j) {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}*/
