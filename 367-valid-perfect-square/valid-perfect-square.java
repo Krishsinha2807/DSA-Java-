dc09c2cb0610449c42899da009ae1f7415d6094b
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-08-11 08:43:00 +0530

    Time: 2444 ms (5.02%) | Memory: 41.5 MB (98.63%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPerfectSquare(long num) {
+        for(long i=1;i<=num;i++)
+        {
+            if(i*i==num)return true;  
+        }
+        return false;
+    }
+}
