a060a338ed6818872d00c649a9b035905c831160
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-08-11 08:54:29 +0530

    Time: 21 ms (71.54%) | Memory: 63.4 MB (97.01%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -22,12 +22,12 @@ class Solution {
         }
       */
         Arrays.sort(arr); 
-        for (int i = 0; i < arr.length - 1; i++) {
-            min = Math.min(min, arr[i + 1] - arr[i]);
+        for (int i=0;i<arr.length-1;i++) {
+            min=Math.min(min,arr[i + 1]-arr[i]);
         }
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i + 1] - arr[i] == min) {
-                result.add(Arrays.asList(arr[i], arr[i + 1]));
+        for (int i=0;i<arr.length-1;i++) {
+            if (arr[i+1]-arr[i]==min) {
+                result.add(Arrays.asList(arr[i],arr[i + 1]));
             }
         }
         return result;
